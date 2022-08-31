38c6fd32ec559bff2df69c8e88f3ee51dfcc6782
Dmitriy Biryulin AN-34 <filin2hat@yandex.ru>
2022-08-31 20:48:07 +0300

    final

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,18 @@
-import classes.Comments
-import classes.Music
-import classes.MusicAttach
-import classes.Post
+import classes.*
 import services.WallService
 
 fun main() {
     WallService.add(Post(text = "First post!", comment = null, attach = null))
     WallService.add(Post(text = "Hello!", comment = Comments(1), attach = null))
-    WallService.update(Post(id = 2, text = "Second post!", comment = Comments(2, true, true, false), attach = null))
+    WallService.update(Post(id = 2, text = "Second post!", comment = Comments(2), attach = null))
     val postWithAttachment =
-        Post(text = "add music attach", attach = arrayOf(MusicAttach(Music(1, 1, 1, 11))), comment = null)
+        Post(
+            text = "add music attach",
+            attach = arrayOf(
+                MusicAttach(Music(1, 1, 1, 11)),
+                VideoAttach(Video(2, 2, 2, 22))
+            ), comment = Comments(3)
+        )
     WallService.add(postWithAttachment)
     WallService.getAllPosts()
     println()
