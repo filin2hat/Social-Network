58e94482ea5652501a75ab07bf24d4c0ecf40229
Dmitriy Biryulin AN-34 <filin2hat@yandex.ru>
2022-09-03 07:28:28 +0300

    add exceptions and class Report

---
+++ src/main/kotlin/classes/Report.kt
@@ -0,0 +1,8 @@
+package classes
+
+data class Report(
+    val ownerId: Int = 0,
+    val commentIs: Int = 0,
+    val reason: Byte?
+) {
+}
---
+++ src/main/kotlin/exceptions/CommentNotFoundException.kt
@@ -0,0 +1,4 @@
+package exceptions
+
+class CommentNotFoundException : RuntimeException ("Comment not found!") {
+}
