f79d765325433362926a9b54b154b13facd37cfc
Dmtry Biryulin <filin2hat@yandex.ru>
2022-09-06 20:03:15 +0300

    final

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,23 +1,12 @@
 import classes.*
+import services.NoteService
 import services.WallService
 
-val post1 = WallService.add(Post(text = "First post!", comment = null, attach = null))
-val post2 = WallService.add(Post(text = "Hello!", comment = null, attach = null))
 fun main() {
-    WallService.update(
-        Post(
-            id = 2,
-            text = "Second post!",
-            comment = Comments(2),
-            attach = arrayOf(
-                MusicAttach(Music(1, 1, 1, 11)),
-                VideoAttach(Video(2, 2, 2, 22))
-            )
-        )
-    )
-    WallService.createComment(1, Comments())
-    WallService.addReport(1, 0, Report(reason = 1))
-    WallService.delete(post1)
-    WallService.getAllPosts()
-    WallService.print(post2)
+    val note = NoteService.add(Note(text = "press button"))
+    NoteService.addComment(1, Comments())
+
+    println(note.isDelete)
+    val result1 = NoteService.delete(note)
+    println(note.isDelete)
 }
